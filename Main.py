4ae288345a13691c508b99e2f9a19696f623c3af
Ephraim-Cheaye-22 <833955@mystma.org>
2021-09-13 09:42:42 -0500

    Top 5 Movies

---
+++ Main.py
@@ -0,0 +1,14 @@
+#Print 1st Movie
+print("Into the Spider Verse")
+
+#Print 2nd movie
+print("Batman the Dark Knight")
+
+#Print 3rd movie
+print("The Black Pearl")
+
+#Print 4th movie
+print("Black Panther")
+
+#Print 5th movie
+print("Cruella")
