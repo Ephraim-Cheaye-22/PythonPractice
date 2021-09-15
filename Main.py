1480442e4b37c96c85852280f864282e866b66f0
Ephraim-Cheaye-22 <833955@mystma.org>
2021-09-15 09:12:53 -0500

    Five Fast Food Places

--- Main.py
+++ Main.py
@@ -16,3 +16,22 @@ def topFiveMovies():
 
 print("these are my top five movies")
 topFiveMovies()
+
+def favoriteFastFoodPlaces():
+    #Print the First Place
+    print("Five Guys")
+
+    #Print the Second Place
+    print("Dairy Queen")
+
+    #Print the Third Place
+    print("Culvers")
+
+    #Print the Fourth Place
+    print("Subway")
+
+    #Print the Fifth Place
+    print("Wendys")
+
+print("these are my favorite Fast Food Places")
+favoriteFastFoodPlaces()
